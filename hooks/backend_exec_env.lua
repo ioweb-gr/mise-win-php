4a2ce4b996869980b709ae41b7fe750a3637ebe5
IOWEB TECHNOLOGIES <info@ioweb.gr>
2026-04-15 13:36:09 +0300

    fix: add bin_paths to backend_exec_env for Mise to find binary

--- hooks/backend_exec_env.lua
+++ hooks/backend_exec_env.lua
@@ -3,6 +3,7 @@
 function PLUGIN:BackendExecEnv(ctx)
     -- PHP binaries on Windows are located in the root installation folder
     return {
+        bin_paths = { ctx.install_path },
         env_vars = {
             { key = "PATH", value = ctx.install_path }
         }
