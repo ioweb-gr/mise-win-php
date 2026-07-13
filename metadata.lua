88cb2bacf272cc40c722762c174afd725eae116d
IOWEB TECHNOLOGIES <info@ioweb.gr>
2026-07-13 10:40:26 +0300

    chore: bump version to 1.3.1

--- metadata.lua
+++ metadata.lua
@@ -1,6 +1,6 @@
 PLUGIN = {
     name = "win-php",
-    version = "1.3.0",
+    version = "1.3.1",
     description = "Mise backend plugin for installing PHP on Windows using pre-built binaries",
     author = "ioweb-gr",
     homepage = "https://github.com/ioweb-gr/mise-win-php",
